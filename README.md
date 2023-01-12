dcefbbc2da05331351fa740cc6cdd2a75ce90736
OleksandrB93 <barabashsan4ik@gmail.com>
2023-01-12 17:37:31 +0200

    first comit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+https://monosnap.com/file/NYkvaTmJxQDmZ3E0ktt9VsyhOUJZ85
+
+# Отримуємо контакт по id
+https://monosnap.com/file/bqCRjZoA2QQ09UPRqi9ejJ3HQW022O
+
+# Додаємо контакт
+https://monosnap.com/file/6AOFJ6KcM68KrVUDonn2i1NFRwmfAQ
+
+# Видаляємо контакт
+https://monosnap.com/file/ZOPPePuYFZGFSsNIWshodyt4OMUEmQinfo about this project
+info about this project
---
+++ contacts.js
@@ -0,0 +1,62 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const { nanoid } = require("nanoid");
+const id = nanoid();
+
+const contactsPath = path.resolve(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  try {
+    const fileData = await fs.readFile(contactsPath);
+    const data = JSON.parse(fileData.toString());
+    return data;
+  } catch (error) {
+    console.log("listContacts", error);
+  }
+}
+
+async function getContactById(contactID) {
+  try {
+    const data = await listContacts();
+    const getContact = data.find(
+      (contact) => contact.id === contactID.toString()
+    );
+    console.log(getContact);
+    return getContact;
+  } catch (error) {
+    console.log("getContactById", error);
+  }
+}
+
+async function removeContact(contactID) {
+  try {
+    const data = await listContacts();
+    const deleteContact = data.filter((contact) => contact.id !== contactID);
+
+    await fs.writeFile(contactsPath, JSON.stringify(deleteContact, null, 2));
+    console.log(`User with id: ${contactID} has been deleted`);
+  } catch (error) {
+    console.log("removeConract", error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await listContacts();
+    const contact = { id, name, email, phone };
+    data.push(contact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+    console.log(`User with name: ${contact.name} has been added`);
+  } catch (error) {
+    console.log("addContact", error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
